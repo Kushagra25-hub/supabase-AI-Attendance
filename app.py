dca1acda182f9cb68324e9ecf29cdd0a539f12fe
Kushagra Srivastava <kushagrasri2004@gmail.com>
2026-05-29 17:20:00 +0530

    Updation

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
 import streamlit as st
 
+st.write("QUERY PARAMS:", st.query_params)
+
 from src.screens.home_screen import home_screen
 from src.screens.teacher_screen import teacher_screen
 from src.screens.student_screen import student_screen
